404015fd06807eebd604f1d277d83ecd490a7796
Nikhil-1426 <parkar.nikhil1426@gmail.com>
2024-07-27 15:29:25 +0530

    Loading Screen Updated

--- lib/loading_screen.dart
+++ lib/loading_screen.dart
@@ -1,3 +1,67 @@
+// import 'package:flutter/material.dart';
+
+// class LoadingScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.black,
+//       body: Center(
+//         child: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: [
+//               Text(
+//                 'Welcome to\nPark.io',
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(
+//                   color: Colors.white,
+//                   fontSize: 32,
+//                   fontWeight: FontWeight.bold,
+//                   fontFamily: 'Serif',
+//                 ),
+//               ),
+//               SizedBox(height: 20),
+//               Text(
+//                 'Your Parking Sidekick',
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(
+//                   color: Colors.white,
+//                   fontSize: 16,
+//                   fontFamily: 'Monospace',
+//                 ),
+//               ),
+//               SizedBox(height: 40),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   Navigator.pushNamed(context, '/signup');
+//                 },
+//                 style: ElevatedButton.styleFrom(
+//                   backgroundColor: Colors.grey[850], // background color
+//                   foregroundColor: Colors.white, // text color
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(30.0),
+//                   ),
+//                 ),
+//                 child: Padding(
+//                   padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+//                   child: Text(
+//                     'Get Started',
+//                     style: TextStyle(fontSize: 16),
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
 import 'package:flutter/material.dart';
 
 class LoadingScreen extends StatelessWidget {
@@ -5,58 +69,73 @@ class LoadingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Welcome to\nPark.io',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Serif',
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Your Parking Sidekick',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontFamily: 'Monospace',
-                ),
-              ),
-              SizedBox(height: 40),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/signup');
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.grey[850], // background color
-                  foregroundColor: Colors.white, // text color
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start, // Align widgets to the left
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 150, 0, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Welcome to',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 56,
+                    fontFamily: 'Poppins', // Adjust the font family as per your requirements
                   ),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-                  child: Text(
-                    'Get Started',
-                    style: TextStyle(fontSize: 16),
+                Text(
+                  'ParkOn',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 80,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Poppins', // Adjust the font family as per your requirements
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 20), // Reduced space between lines of text
+                Text(
+                  'Your Parking Sidekick',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 24,
+                    fontFamily: 'Roboto', // Adjust the font family as per your requirements
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+          Spacer(), // Pushes the button to the center of the page
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/signup');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange, // Button background color
+                foregroundColor: Colors.white, // Button text color
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+                child: Text(
+                  'Get Started',
+                  style: TextStyle(fontSize: 16,color: Colors.black,fontWeight: FontWeight.bold,),
+      
+                ),
+              ),
+            ),
+          ),
+          Spacer(), // Ensures the button remains centered vertically
+        ],
       ),
     );
   }
 }
-
