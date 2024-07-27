b43ac15dd187f9a5f8b013b3108194db69a23f62
Nikhil-1426 <parkar.nikhil1426@gmail.com>
2024-07-27 15:48:46 +0530

    Settings Page Updated

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,3 +1,169 @@
+// import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+// class SettingsScreen extends StatefulWidget {
+//   @override
+//   _SettingsScreenState createState() => _SettingsScreenState();
+// }
+
+// class _SettingsScreenState extends State<SettingsScreen> {
+//   final TextEditingController _nameController = TextEditingController();
+//   final TextEditingController _ageController = TextEditingController();
+//   final TextEditingController _licenseController = TextEditingController();
+//   final FirebaseAuth _auth = FirebaseAuth.instance;
+//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _loadUserProfile();
+//   }
+
+//   void _loadUserProfile() async {
+//     User? user = _auth.currentUser;
+//     if (user != null) {
+//       DocumentSnapshot userProfile = await _firestore.collection('users').doc(user.uid).get();
+//       if (userProfile.exists) {
+//         setState(() {
+//           _nameController.text = userProfile['name'];
+//           _ageController.text = userProfile['age'].toString();
+//           _licenseController.text = userProfile['license'];
+//         });
+//       }
+//     }
+//   }
+
+//   void _saveUserProfile() async {
+//     User? user = _auth.currentUser;
+//     if (user != null) {
+//       await _firestore.collection('users').doc(user.uid).update({
+//         'name': _nameController.text,
+//         'age': int.tryParse(_ageController.text) ?? 0,
+//         'license': _licenseController.text,
+//       });
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         leading: IconButton(
+//           icon: Icon(Icons.arrow_back, color: Colors.white), // White back icon
+//           onPressed: () {
+//             Navigator.pop(context);
+//           },
+//         ),
+//         title: Text('Settings', style: TextStyle(color: Colors.white)), // White text color
+//         backgroundColor: Colors.black,
+//       ),
+//       body: SingleChildScrollView(
+//         child: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Column(
+//             children: [
+//               CircleAvatar(
+//                 radius: 50,
+//                 backgroundColor: Colors.grey[850],
+//                 child: Icon(Icons.person, size: 50, color: Colors.white),
+//               ),
+//               SizedBox(height: 20),
+//               TextField(
+//                 controller: _nameController,
+//                 decoration: InputDecoration(
+//                   labelText: 'Name',
+//                   labelStyle: TextStyle(color: Colors.white),
+//                   filled: true,
+//                   fillColor: Colors.grey[850],
+//                   border: OutlineInputBorder(
+//                     borderRadius: BorderRadius.circular(30.0),
+//                   ),
+//                 ),
+//                 style: TextStyle(color: Colors.white),
+//               ),
+//               SizedBox(height: 10),
+//               TextField(
+//                 controller: _ageController,
+//                 decoration: InputDecoration(
+//                   labelText: 'Age',
+//                   labelStyle: TextStyle(color: Colors.white),
+//                   filled: true,
+//                   fillColor: Colors.grey[850],
+//                   border: OutlineInputBorder(
+//                     borderRadius: BorderRadius.circular(30.0),
+//                   ),
+//                 ),
+//                 style: TextStyle(color: Colors.white),
+//                 keyboardType: TextInputType.number,
+//               ),
+//               SizedBox(height: 10),
+//               TextField(
+//                 controller: _licenseController,
+//                 decoration: InputDecoration(
+//                   labelText: 'Car License Number',
+//                   labelStyle: TextStyle(color: Colors.white),
+//                   filled: true,
+//                   fillColor: Colors.grey[850],
+//                   border: OutlineInputBorder(
+//                     borderRadius: BorderRadius.circular(30.0),
+//                   ),
+//                 ),
+//                 style: TextStyle(color: Colors.white),
+//               ),
+//               SizedBox(height: 20),
+//               ElevatedButton(
+//                 onPressed: _saveUserProfile,
+//                 style: ElevatedButton.styleFrom(
+//                   backgroundColor: Colors.grey[850], // background color
+//                   foregroundColor: Colors.white, // text color
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(30.0),
+//                   ),
+//                 ),
+//                 child: Padding(
+//                   padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+//                   child: Text(
+//                     'Save',
+//                     style: TextStyle(fontSize: 16),
+//                   ),
+//                 ),
+//               ),
+//               SizedBox(height: 20),
+//               Divider(color: Colors.grey),
+//               SizedBox(height: 20),
+//               ElevatedButton(
+//                 onPressed: () async {
+//                   await FirebaseAuth.instance.signOut();
+//                   Navigator.pushReplacementNamed(context, '/login');
+//                 },
+//                 style: ElevatedButton.styleFrom(
+//                   backgroundColor: Colors.redAccent, // background color
+//                   foregroundColor: Colors.white, // text color
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(30.0),
+//                   ),
+//                 ),
+//                 child: Padding(
+//                   padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+//                   child: Text(
+//                     'Sign Out',
+//                     style: TextStyle(fontSize: 16),
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//       backgroundColor: Colors.black,
+//     );
+//   }
+// }
+
+
+
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -11,6 +177,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _ageController = TextEditingController();
   final TextEditingController _licenseController = TextEditingController();
+  final TextEditingController _fastagController = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
@@ -29,6 +196,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
           _nameController.text = userProfile['name'];
           _ageController.text = userProfile['age'].toString();
           _licenseController.text = userProfile['license'];
+          _fastagController.text = userProfile['fastag_info'];
         });
       }
     }
@@ -69,7 +237,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 child: Icon(Icons.person, size: 50, color: Colors.white),
               ),
               SizedBox(height: 20),
-              TextField(
+              TextFormField(
                 controller: _nameController,
                 decoration: InputDecoration(
                   labelText: 'Name',
@@ -83,7 +251,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 style: TextStyle(color: Colors.white),
               ),
               SizedBox(height: 10),
-              TextField(
+              TextFormField(
                 controller: _ageController,
                 decoration: InputDecoration(
                   labelText: 'Age',
@@ -98,10 +266,25 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 keyboardType: TextInputType.number,
               ),
               SizedBox(height: 10),
-              TextField(
+              TextFormField(
                 controller: _licenseController,
                 decoration: InputDecoration(
                   labelText: 'Car License Number',
+                  labelStyle: TextStyle(color: Colors.white),
+                  filled: true,
+                  fillColor: Colors.grey[850],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                controller: _fastagController,
+                readOnly: true,
+                decoration: InputDecoration(
+                  labelText: 'Fastag ID',
                   labelStyle: TextStyle(color: Colors.white),
                   filled: true,
                   fillColor: Colors.grey[850],
@@ -160,5 +343,4 @@ class _SettingsScreenState extends State<SettingsScreen> {
     );
   }
 }
-
 
