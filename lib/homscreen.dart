591598f51cf8b3ca04d17c6e61e22b0b6d6c735f
Nikhil-1426 <parkar.nikhil1426@gmail.com>
2024-07-27 18:39:49 +0530

    Added Maps

--- lib/homscreen.dart
+++ lib/homscreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -10,6 +11,17 @@ class _HomeScreenState extends State<HomeScreen> {
   bool _isFindingParking = false;
   bool _parkingFound = false;
   bool _isFindingRoute = false;
+  bool _showMap = false;
+
+  MapController mapController = MapController(
+    initPosition: GeoPoint(latitude: 47.4358055, longitude: 8.4737324),
+    areaLimit: BoundingBox(
+      east: 10.4922941,
+      north: 47.8084648,
+      south: 45.817995,
+      west: 5.9559113,
+    ),
+  );
 
   void _findParkingSpot() async {
     setState(() {
@@ -30,6 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
     setState(() {
       _isFindingRoute = false;
+      _showMap = true; // Show the map after finding the route
     });
   }
 
@@ -86,114 +99,157 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: _isFindingParking
-              ? CircularProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                )
-              : _parkingFound
-                  ? _isFindingRoute
-                      ? Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.local_parking,
-                              color: Colors.white,
-                              size: 80,
-                            ),
-                            SizedBox(height: 20),
-                            Text(
-                              'Found Parking',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 32,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Serif',
-                              ),
-                            ),
-                            SizedBox(height: 20),
-                            LinearProgressIndicator(
-                              valueColor: AlwaysStoppedAnimation<Color>(Colors.purpleAccent),
-                              backgroundColor: Colors.white,
-                            ),
-                            SizedBox(height: 20),
-                            Text(
-                              'Finding optimal route',
-                              style: TextStyle(
-                                color: Colors.purpleAccent,
-                                fontSize: 16,
-                              ),
-                            ),
-                            SizedBox(height: 20),
-                            ElevatedButton(
-                              onPressed: _cancelFindingRoute,
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Color.fromARGB(255, 133, 126, 125), // background color
-                                foregroundColor: Colors.white, // text color
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30.0),
-                                ),
-                                padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-                              ),
-                              child: Text(
-                                'Cancel',
-                                style: TextStyle(fontSize: 16),
-                              ),
-                            ),
-                          ],
-                        )
-                      : Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Icon(
-                              Icons.local_parking,
-                              color: Colors.white,
-                              size: 80,
-                            ),
-                            SizedBox(height: 20),
-                            Text(
-                              'Parking Found',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 32,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Serif',
-                              ),
-                            ),
-                            SizedBox(height: 20),
-                            ElevatedButton(
-                              onPressed: _findParkingSpot,
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Colors.grey[850], // background color
-                                foregroundColor: Colors.white, // text color
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30.0),
-                                ),
-                                padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
-                              ),
-                              child: Text(
-                                'Find Parking Spot',
-                                style: TextStyle(fontSize: 16),
-                              ),
-                            ),
-                          ],
-                        )
-                  : ElevatedButton(
-                      onPressed: _findParkingSpot,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey[850], // background color
-                        foregroundColor: Colors.white, // text color
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
+          child: _showMap
+              ? OSMFlutter(
+                  controller: mapController,
+                  osmOption: OSMOption(
+                    userTrackingOption: const UserTrackingOption(
+                      enableTracking: true,
+                      unFollowUser: false,
+                    ),
+                    zoomOption: const ZoomOption(
+                      initZoom: 8,
+                      minZoomLevel: 3,
+                      maxZoomLevel: 19,
+                      stepZoom: 1.0,
+                    ),
+                    userLocationMarker: UserLocationMaker(
+                      personMarker: const MarkerIcon(
+                        icon: Icon(
+                          Icons.location_history_rounded,
+                          color: Colors.red,
+                          size: 48,
                         ),
-                        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
                       ),
-                      child: Text(
-                        'Find Parking Spot',
-                        style: TextStyle(fontSize: 16),
+                      directionArrowMarker: const MarkerIcon(
+                        icon: Icon(
+                          Icons.double_arrow,
+                          size: 48,
+                        ),
                       ),
                     ),
+                    roadConfiguration: const RoadOption(
+                      roadColor: Colors.yellowAccent,
+                    ),
+                    markerOption: MarkerOption(
+                      defaultMarker: const MarkerIcon(
+                        icon: Icon(
+                          Icons.local_parking,
+                          color: Colors.blue,
+                          size: 56,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              : _isFindingParking
+                  ? CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                    )
+                  : _parkingFound
+                      ? _isFindingRoute
+                          ? Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.local_parking,
+                                  color: Colors.white,
+                                  size: 80,
+                                ),
+                                SizedBox(height: 20),
+                                Text(
+                                  'Found Parking',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Serif',
+                                  ),
+                                ),
+                                SizedBox(height: 20),
+                                LinearProgressIndicator(
+                                  valueColor: AlwaysStoppedAnimation<Color>(Colors.purpleAccent),
+                                  backgroundColor: Colors.white,
+                                ),
+                                SizedBox(height: 20),
+                                Text(
+                                  'Finding optimal route',
+                                  style: TextStyle(
+                                    color: Colors.purpleAccent,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                SizedBox(height: 20),
+                                ElevatedButton(
+                                  onPressed: _cancelFindingRoute,
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Color.fromARGB(255, 133, 126, 125), // background color
+                                    foregroundColor: Colors.white, // text color
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30.0),
+                                    ),
+                                    padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+                                  ),
+                                  child: Text(
+                                    'Cancel',
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                ),
+                              ],
+                            )
+                          : Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.local_parking,
+                                  color: Colors.white,
+                                  size: 80,
+                                ),
+                                SizedBox(height: 20),
+                                Text(
+                                  'Parking Found',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 32,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Serif',
+                                  ),
+                                ),
+                                SizedBox(height: 20),
+                                ElevatedButton(
+                                  onPressed: _findParkingSpot,
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.grey[850], // background color
+                                    foregroundColor: Colors.white, // text color
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30.0),
+                                    ),
+                                    padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+                                  ),
+                                  child: Text(
+                                    'Find Parking Spot',
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                ),
+                              ],
+                            )
+                      : ElevatedButton(
+                          onPressed: _findParkingSpot,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.grey[850], // background color
+                            foregroundColor: Colors.white, // text color
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                            padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+                          ),
+                          child: Text(
+                            'Find Parking Spot',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
         ),
       ),
     );
